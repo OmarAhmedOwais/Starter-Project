eab36ae07eec45786e6967bf1764147532ec4da4
OmarAhmedOwais <oa1476@fayoum.edu.eg>
2024-07-01 00:42:31 +0300

    update config

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,10 @@ import { IConfig } from '@/types';
 import { validateEnv } from '@/utils';
 import dotenv from "dotenv";
 import path from "path";
-dotenv.config({ path: path.join(__dirname, "../.env") });
+// Determine the environment from the npm script name
+const envFile = `.env.${process.env.NODE_ENV}`;
+// Load the environment variables from the file
+dotenv.config({ path: path.join(__dirname, `../${envFile}`) });
 const { error, value } = validateEnv(process.env);
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
