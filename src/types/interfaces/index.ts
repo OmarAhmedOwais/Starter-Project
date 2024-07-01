a8a69b72ad3fb9cd7c830465ea22522cacb6f70b
OmarAhmed0001 <oa1476@fayoum.edu.eg>
2024-07-01 15:19:35 +0300

    win command

--- src/types/interfaces/index.ts
+++ src/types/interfaces/index.ts
@@ -2,4 +2,3 @@ export * from './user.interface';
 export * from './pagination.interface';
 export * from './query.interface';
 export * from './config.interface';
-export * from './param.interface';
